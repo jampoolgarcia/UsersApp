effe04657db8feb9de216642a8d1c9bc30f9beb3
jampoolG <jampoolgarcia@gmail.com>
2021-07-11 10:45:31 -0400

    added layout template

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -3,8 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styles: [
-  ]
+  styles: [`
+    hr {
+      background-color: #fff;
+      max-width: 65px !important;
+      height: 6px !important;
+      opacity: 1 !important;
+      margin: auto;
+    }
+  `]
 })
 export class LayoutComponent implements OnInit {
 
